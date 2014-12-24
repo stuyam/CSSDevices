eb943a8564c924e257ab6416cb3ccac451f5de90
Stuart Yamartino <stuart@yamartino.com>
2014-12-23 23:41:27 -0500

    Working on the api of the now "cd-smart-loader"

--- devices.js
+++ devices.js
@@ -4,13 +4,13 @@ $(function(){
     var pageSpeed = ifDataExists($this, 'page-speed', 5000);
     var fadeSpeed = ifDataExists($this, 'fade-speed', 1000);
     $('> :gt(0)', $this).hide();
-    if (!$('> :eq(0)', $this).hasClass('cd-loader')) {
+    if ( !$($this).hasClass('cd-smart-loader')) {
       $('> :eq(0)', $this).css('display', 'block');
     }
     setInterval(function(){$('> :first-child',$this).fadeOut(fadeSpeed).next().fadeIn(fadeSpeed).end().appendTo($this);}, pageSpeed);
   })
 
-  $(".cd-loader").each(function() {
+  $(".cd-smart-loader > :first-child").each(function() {
     var $this = $(this);
     $this.on("load", handleLoad);
     if (this.complete) {
@@ -20,7 +20,7 @@ $(function(){
   });
 
   function handleLoad(){
-    var loadSpeed = ifDataExists(this, 'load-speed', 250);
+    var loadSpeed = ifDataExists(this, 'load-in-speed', 250);
     $(this).fadeIn(loadSpeed);
   }
 
