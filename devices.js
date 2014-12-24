e65ecb000249c3163e0ad67a2f3f15b51234f7fd
Stuart Yamartino <stuart@yamartino.com>
2014-12-24 13:22:56 -0500

    cd-screen with multiple children slideshow by default
    Now a cd-screen with multiple children with slideshow by default,
    unless you give the screen the class .cd-no-slideshow to prevent the
    default slideshow.
    
    TODO: Still need to figure out the element that will show with
    .cd-no-slideshow by default

--- devices.js
+++ devices.js
@@ -1,14 +1,17 @@
 $(function(){
-  $('.cd-slideshow').each(function(){
+  if( $('.cd-screen').children().length > 1){}
+  $('.cd-screen').each(function(){
     var $this = this;
-    var pageSpeed = ifDataExists($this, 'page-speed', 5000);
-    var fadeSpeed = ifDataExists($this, 'fade-speed', 1000);
-    $('> :gt(0)', $this).hide();
-    if ( !$($this).hasClass('cd-smart-loader')) {
-      $('> :eq(0)', $this).css('display', 'block');
+    if( $($this).children().length > 1 && !$($this).hasClass('cd-no-slideshow')){
+      var pageSpeed = ifDataExists($this, 'page-speed', 5000);
+      var fadeSpeed = ifDataExists($this, 'fade-speed', 1000);
+      $('> :gt(0)', $this).hide();
+      if ( !$($this).hasClass('cd-smart-loader')) {
+        $('> :eq(0)', $this).css('display', 'block');
+      }
+      setInterval(function(){$('> :first-child',$this).fadeOut(fadeSpeed).next().fadeIn(fadeSpeed).end().appendTo($this);}, pageSpeed);
     }
-    setInterval(function(){$('> :first-child',$this).fadeOut(fadeSpeed).next().fadeIn(fadeSpeed).end().appendTo($this);}, pageSpeed);
-  })
+  });
 
   $(".cd-smart-loader > :first-child").each(function() {
     var $this = $(this);
